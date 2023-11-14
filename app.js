b40e7e2495aaa985a3a4199070054f0d64f0717d
Camilay3 <camila.santiago026@gmail.com>
2023-11-14 14:54:06 -0300

    Rotas 1.2#

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const exphbs = require('express-handlebars');
+const handlebars = exphbs.create({});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/view/index.html');
+    res.render('formulario');
 })
 
 app.listen (3000, () => {
